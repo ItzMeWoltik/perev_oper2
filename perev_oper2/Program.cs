f59c642cff586cb142baff79decd1d05b20e94ee
ItzMeWoltik <itzmewoltik@gmail.com>
2024-03-12 08:35:26 +0200

    123

--- perev_oper2/Program.cs
+++ perev_oper2/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        List<Weight> weights = new List<Weight>
+        {
+            new Weight { Mass = 10, Color = ConsoleColor.Red, Size = 5 },
+            new Weight { Mass = 5, Color = ConsoleColor.Green, Size = 3 },
+            new Weight { Mass = 8, Color = ConsoleColor.Blue, Size = 4 },
+            new Weight { Mass = 3, Color = ConsoleColor.Yellow, Size = 2 },
+            new Weight { Mass = 12, Color = ConsoleColor.Magenta, Size = 6 }
+        };
+
+        Console.WriteLine("Sorted by Mass:");
+        weights.Sort();
+        foreach (Weight weight in weights)
+        {
+            Console.WriteLine($"Mass: {weight.Mass}, Color: {weight.Color}, Size: {weight.Size}");
+        }
+
+        Console.WriteLine("\nSorted by Size:");
+        weights.Sort((x, y) => x.Size.CompareTo(y.Size));
+        foreach (Weight weight in weights)
+        {
+            Console.WriteLine($"Mass: {weight.Mass}, Color: {weight.Color}, Size: {weight.Size}");
+        }
+
+        Console.WriteLine("\nSorted by Color:");
+        weights.Sort((x, y) => x.Color.CompareTo(y.Color));
+        foreach (Weight weight in weights)
+        {
+            Console.WriteLine($"Mass: {weight.Mass}, Color: {weight.Color}, Size: {weight.Size}");
+        }
+
+        Console.ReadLine();
+    }
+}
--- perev_oper2/Weight.cs
+++ perev_oper2/Weight.cs
@@ -1,6 +1,60 @@
-﻿namespace perev_oper2;
-
-public class Weight
+﻿public class Weight : IComparable<Weight>, IComparer<Weight>
 {
-    
+    public int Mass { get; set; }
+    public ConsoleColor Color { get; set; }
+    public int Size { get; set; }
+
+    public override bool Equals(object obj)
+    {
+        if (obj == null || GetType() != obj.GetType())
+            return false;
+
+        Weight other = (Weight)obj;
+        return Mass == other.Mass && Color == other.Color && Size == other.Size;
+    }
+
+    public override int GetHashCode()
+    {
+        return Mass.GetHashCode() ^ Color.GetHashCode() ^ Size.GetHashCode();
+    }
+
+    public static bool operator ==(Weight w1, Weight w2)
+    {
+        return w1.Equals(w2);
+    }
+
+    public static bool operator !=(Weight w1, Weight w2)
+    {
+        return !w1.Equals(w2);
+    }
+
+    public static bool operator >(Weight w1, Weight w2)
+    {
+        return w1.Mass > w2.Mass;
+    }
+
+    public static bool operator <(Weight w1, Weight w2)
+    {
+        return w1.Mass < w2.Mass;
+    }
+
+    public static bool operator >=(Weight w1, Weight w2)
+    {
+        return w1.Mass >= w2.Mass;
+    }
+
+    public static bool operator <=(Weight w1, Weight w2)
+    {
+        return w1.Mass <= w2.Mass;
+    }
+
+    public int CompareTo(Weight other)
+    {
+        return Mass.CompareTo(other.Mass);
+    }
+
+    public int Compare(Weight x, Weight y)
+    {
+        return x.Mass.CompareTo(y.Mass);
+    }
 }
